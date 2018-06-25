ac08b643028f44f1f50c0176ac561e0a4eda2870
benzomatic <mr.benz@freenet.de>
2018-06-25 16:40:23 +0200

    Update rugby.lua to only allow passing to your side and backwards..
    .. so that you can't just lazily shoot across every open map a billion times over.

--- scripts/gamemods/rugby.lua
+++ scripts/gamemods/rugby.lua
@@ -1,10 +1,31 @@
 --[[
+
   Rugby mode: pass the flag you hold to a teammate by shooting at him with rifle
+  Addition for CTF: Only allow tactical side and backwards passes to prevent open maps from being finished too early through effortless long-range passes.
+
 ]]--
 
 local module = {}
 
 local dodamagehook
+
+local ents, vec3, playermsg = require"std.ents", require"utils.vec3", require"std.playermsg"
+local tolerance = 60 -- add a slight tolerance
+local function allowpass(actor, target)
+  if server.m_hold or server.m_protect then return true end -- only restrict passing in ctf
+  for i, _, ment in ents.enum(server.FLAG) do
+    local flagteam = server.ctfflagteam(ment.attr2)
+    if actor.team == flagteam then
+      local dist1, dist2 = vec3(ment.o):dist(actor.state.o), vec3(ment.o):dist(target.state.o)
+      if dist1 <= (dist2 + tolerance) then 
+        return true 
+      end
+    end
+  end
+  playermsg("\f6Info\f7: You can only do \f6tactical side or backwards passes\f7! This is actual Rugby, you still gotta \f6run\f7!!", actor)
+  return false
+end
+
 function module.on(state)
   if dodamagehook then spaghetti.removehook(dodamagehook) dodamagehook = nil end
   if not state then return end
@@ -14,6 +35,7 @@ function module.on(state)
     for i = 0, flags:length() - 1 do if flags[i].owner == info.actor.clientnum then actorflags[i] = true end end
     if not next(actorflags) then return end
     info.skip = true
+    if not allowpass(info.actor, info.target) then return end
     for flag in pairs(actorflags) do
       server.ctfmode:returnflag(flag, 0)
       server.ctfmode:takeflag(info.target, flag, flags[flag].version)
